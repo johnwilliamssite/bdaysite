19a0d5640d43dc6974b39586bb429cea4b82f10b
Robert Burkhardt <rgeorgebu@gmail.com>
2019-09-03 21:52:06 -0500

    Added check for 2nd birthday

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function checkDateLoop() {
     setInterval(function() {
         const currentDate = new Date();
-        if (currentDate.getDate() === 10 && currentDate.getMonth() === 7) {
+        if ((currentDate.getDate() === 10 && currentDate.getMonth() === 7) || 
+        (currentDate.getDate() === 4 && currentDate.getMonth() === 8)) {
             document.getElementById("answer").innerText = "Yes"
         } else {
             document.getElementById("answer").innerText = "No"
